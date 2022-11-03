0f565304a7a30bc117c1e373a1937bff7de5ba28
Kirill <kuzminkk01@gmail.com>
2022-11-03 14:36:14 +0400

    Add async logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -9,20 +9,14 @@ import (
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"net/url"
+	"os"
+	"sync"
 )
 
 func main() {
 	if err := InitConfig(); err != nil {
 		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
-
-	flag.Parse()
-	if flag.NArg() < 1 {
-		logrus.Fatal("not enough arguments")
-	}
-
-	videoUrl := flag.Arg(0)
-	link := getLink(videoUrl)
 
 	address := fmt.Sprintf("%s:%s", viper.GetString("host"), viper.GetString("port"))
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
@@ -31,12 +25,38 @@ func main() {
 	}
 
 	client := api.NewThumbnailsClient(conn)
+
+	asyncFlag := flag.Bool("async", false, "implements asynchronous execution")
+	flag.Parse()
+
+	if *asyncFlag {
+		var wg sync.WaitGroup
+		args := os.Args[2:]
+		for _, arg := range args {
+			wg.Add(1)
+			go requestServer(arg, client, &wg)
+		}
+
+		wg.Wait()
+	} else {
+		args := os.Args[1:]
+		for _, arg := range args {
+			requestServer(arg, client, nil)
+		}
+	}
+}
+
+func requestServer(videoUrl string, client api.ThumbnailsClient, wg *sync.WaitGroup) {
+	if wg != nil {
+		defer wg.Done()
+	}
+	link := getLink(videoUrl)
 	response, err := client.Get(context.Background(), &api.GetRequest{Link: link})
 	if err != nil {
-		logrus.Fatalf("error while requesting GRPC-server: %s", err.Error())
+		logrus.Fatalf("error while requesting gRPC-server: %s", err.Error())
 	}
 
-	fmt.Printf("%s\n", response.Thumbnail)
+	fmt.Printf("Thumbnail-file of video %s: %s\n", videoUrl, response.Thumbnail)
 }
 
 func getLink(videoUrl string) string {
@@ -58,3 +78,5 @@ func InitConfig() error {
 	viper.SetConfigName("config")
 	return viper.ReadInConfig()
 }
+
+//go run cmd/client/main.go --async "https://www.youtube.com/watch?v=qu3Vpdnndi4&ab_channel=Rozetked" "https://www.youtube.com/watch?v=2maIPAWo-UM&ab_channel=Rozetked" "https://www.youtube.com/watch?v=KnINsmXT9_c&ab_channel=Droider"
